85d84a51f00a30b09c7e571f975eadc0062c7921
duantihua <duantihua@gmail.com>
2013-07-20 18:53:24 +0800

    update version number to 4.0.0

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.beangle.commons"
-  val buildVersion = "4.0.0-SNAPSHOT"
+  val buildVersion = "4.0.0"
   val buildScalaVersion = "2.10.2"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
