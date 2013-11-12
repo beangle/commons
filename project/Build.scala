d66423be93dc262afc45205a55fece7728e09e7f
duantihua <duantihua@gmail.com>
2013-11-12 12:35:33 +0800

    start 4.0.2-snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "org.beangle.commons"
-  val buildVersion = "4.0.1"
+  val buildVersion = "4.0.2-SNAPSHOT"
   val buildScalaVersion = "2.10.2"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
