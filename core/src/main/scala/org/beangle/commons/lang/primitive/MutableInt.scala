d7a138442593396ba80c91338548bcb955c3a341
chaostone <duantihua@gmail.com>
2014-09-09 00:14:04 +0800

    remove inline on mutableint

--- core/src/main/scala/org/beangle/commons/lang/primitive/MutableInt.scala
+++ core/src/main/scala/org/beangle/commons/lang/primitive/MutableInt.scala
@@ -2,13 +2,11 @@ package org.beangle.commons.lang.primitive
 
 class MutableInt(var value: Int=0) {
 
-  @inline
   def increment(): Int = {
     value += 1
     value
   }
 
-  @inline
   def decrement(): Int = {
     value -= 1
     value
