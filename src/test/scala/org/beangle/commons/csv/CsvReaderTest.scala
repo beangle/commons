124f092b9edf991d5866c390e5d9f83012f76f8f
段体华 <duantihua@gmail.com>
2024-04-07 05:48:17 +0800

    add ProxyResolver

---
+++ src/test/scala/org/beangle/commons/csv/CsvReaderTest.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2005, The Beangle Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.beangle.commons.csv
+
+import java.io.StringReader
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class CsvReaderTest extends AnyFunSpec with Matchers {
+
+  var reader: CsvReader = buildReader()
+
+  def buildReader(): CsvReader = {
+    val sb = new StringBuilder()
+    sb.append("a,b,c").append("\n")
+    sb.append("a,\"b,b,b\",c").append("\n")
+    sb.append(",,").append("\n")
+    sb.append("a,\"PO Box 123,\nKippax,ACT. 2615.\nAustralia\",d.\n")
+    sb.append("\"Glen \"\"The Man\"\" Smith\",Athlete,Developer\n")
+    sb.append("\"\"\"\"\"\",\"test\"\n")
+    sb.append("\"a\nb\",b,\"\nd\",e\n")
+    new CsvReader(new StringReader(sb.toString))
+  }
+
+  /**
+   * Tests iterating over a reader.
+   */
+  describe("CsvReader") {
+    it("Read next") {
+      var nextLine = reader.readNext()
+      nextLine(0) should equal("a")
+      nextLine(1) should equal("b")
+      nextLine(2) should equal("c")
+      nextLine = reader.readNext()
+      nextLine(0) should equal("a")
+      nextLine(1) should equal("b,b,b")
+      nextLine(2) should equal("c")
+      nextLine = reader.readNext()
+      nextLine.length should be(3)
+      nextLine = reader.readNext()
+      nextLine.length should be(3)
+      nextLine = reader.readNext()
+      nextLine(0) should equal("Glen \"The Man\" Smith")
+      nextLine = reader.readNext()
+      nextLine(0) should equal("\"\"")
+      nextLine(1) should equal("test")
+      nextLine = reader.readNext()
+      nextLine.length should be(4)
+      reader.readNext() should be(null)
+    }
+  }
+}
