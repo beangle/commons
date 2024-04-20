7c6a6e9afc934e87fc19d0f898d703a65baf6caf
段体华 <duantihua@gmail.com>
2024-04-20 08:58:19 +0800

    add Networks

---
+++ src/main/scala/org/beangle/commons/net/Networks.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2005, The Beangle Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.beangle.commons.net
+
+import org.beangle.commons.collection.Collections
+
+import java.net.*
+
+object Networks {
+
+  def url(l: String): URL = URI.create(l).toURL
+
+  def uri(l: String): URI = URI.create(l)
+
+  def openURL(l: String): URLConnection = url(l).openConnection()
+
+  def localIPs: Set[String] = {
+    val niEnum = NetworkInterface.getNetworkInterfaces
+    val ips = Collections.newBuffer[String]("127.0.0.1")
+    while (niEnum.hasMoreElements) {
+      val ni = niEnum.nextElement()
+      if (ni.isUp && !ni.isLoopback) {
+        val ipEnum = ni.getInetAddresses
+        while (ipEnum.hasMoreElements) {
+          ipEnum.nextElement() match {
+            case ip: Inet4Address => ips += ip.getHostAddress
+            case _ =>
+          }
+        }
+      }
+    }
+    ips.toSet
+  }
+
+  def tryOpen(l: String): Option[URL] = {
+    try {
+      val rs = url(l)
+      val conn = rs.openConnection
+      conn.connect()
+      Some(rs)
+    } catch {
+      case _: Throwable => None
+    }
+  }
+}
